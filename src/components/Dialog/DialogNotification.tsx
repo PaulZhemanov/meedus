cd855e7b4434a846b8dcb5a4e073fd43e8b19ebf
Paul <zhpv96@gmail.com>
2023-10-31 20:13:27 +0300

    added prettierrc file

--- src/components/Dialog/DialogNotification.tsx
+++ src/components/Dialog/DialogNotification.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {IDialogPropTypes} from "rc-dialog/lib/IDialogPropTypes";
+import { IDialogPropTypes } from "rc-dialog/lib/IDialogPropTypes";
 import Dialog from "@components/Dialog/Dialog";
-import {Column} from "../Flex";
-import {ReactComponent as SuccessIcon} from "@src/assets/icons/successLarge.svg";
+import { Column } from "../Flex";
+import { ReactComponent as SuccessIcon } from "@src/assets/icons/successLarge.svg";
 import SizedBox from "@components/SizedBox";
 import Text from "@components/Text";
 import styled from "@emotion/styled";
@@ -10,7 +10,6 @@ import Button from "@components/Button";
 import NftPreview from "@screens/AchievementsScreen/NftPreview";
 import useElementSize from "@src/hooks/useElementSize";
 // import badge from "@screens/AchievementsScreen/Badge";
-
 
 const Root = styled(Column)`
   text-align: center;
@@ -34,7 +33,7 @@ const Domain = styled.div`
 
 const ButtonsContainer = styled.div<{ direction?: "row" | "column" }>`
   display: flex;
-  flex-direction: ${({direction}) => direction ?? "column"};
+  flex-direction: ${({ direction }) => direction ?? "column"};
   width: 100%;
   margin: -4px;
 
@@ -44,129 +43,123 @@ const ButtonsContainer = styled.div<{ direction?: "row" | "column" }>`
 `;
 
 export interface IDialogNotificationProps extends IDialogPropTypes {
-	title?: string;
-	description?: string;
-	domain?: string;
-	icon?: JSX.Element;
-	type?: "success" | "error" | "warning";
-	buttons?: React.FC[];
-	buttonsDirection?: "row" | "column";
+  title?: string;
+  description?: string;
+  domain?: string;
+  icon?: JSX.Element;
+  type?: "success" | "error" | "warning";
+  buttons?: React.FC[];
+  buttonsDirection?: "row" | "column";
 }
 
 const DialogNotification: React.FC<IDialogNotificationProps> = ({
-	                                                                title,
-	                                                                description,
-	                                                                icon,
-	                                                                type = "success",
-	                                                                buttonsDirection = "column",
-	                                                                buttons = [],
-	                                                                domain,
-	                                                                ...rest
-                                                                }) => {
-	const [squareRef, {width}] = useElementSize();
+  title,
+  description,
+  icon,
+  type = "success",
+  buttonsDirection = "column",
+  buttons = [],
+  domain,
+  ...rest
+}) => {
+  const [squareRef, { width }] = useElementSize();
 
-	return (
-		<Dialog bodyStyle={{padding: "40px"}} {...rest}>
-			<Root alignItems="center" crossAxisSize="max">
-				<SizedBox height={16}/>
-				<>
-					{icon != null ? (
-						<React.Fragment>{icon}</React.Fragment>
-					) : (
-						<React.Fragment>
-							{type === "success" && <SuccessIcon/>}
-						</React.Fragment>
-					)}
-				</>
-				<SizedBox height={8}/>
-				<Text className="title">{title}</Text>
-					<NftPreview
-						style={{height: `${width}px`}}
-						src='https://bafybeib3pth6j3wmt5a4xdmx2zoc7hkykhqewknhurmj3s7v7p6wvsbonq.ipfs.nftstorage.link/'
-					/>
-				<Column>
-				{domain && <Domain>{domain}</Domain>}
-				{description && (
-					<Text style={{marginTop: 8}} weight={500} size="medium">
-						{description}
-					</Text>
-				)}
-				<SizedBox height={16}/>
-				{buttons.length > 0 && (
-					<ButtonsContainer style={{flexDirection: buttonsDirection}}>
-						{buttons?.map((Component, index) => (
-							<Component key="index"/>
-						))}
-					</ButtonsContainer>
-				)}
-				</Column>
-				<SizedBox height={24}/>
-			</Root>
-		</Dialog>
-
-	);
+  return (
+    <Dialog bodyStyle={{ padding: "40px" }} {...rest}>
+      <Root alignItems="center" crossAxisSize="max">
+        <SizedBox height={16} />
+        <>
+          {icon != null ? (
+            <React.Fragment>{icon}</React.Fragment>
+          ) : (
+            <React.Fragment>{type === "success" && <SuccessIcon />}</React.Fragment>
+          )}
+        </>
+        <SizedBox height={8} />
+        <Text className="title">{title}</Text>
+        <NftPreview
+          style={{ height: `${width}px` }}
+          src="https://bafybeib3pth6j3wmt5a4xdmx2zoc7hkykhqewknhurmj3s7v7p6wvsbonq.ipfs.nftstorage.link/"
+        />
+        <Column>
+          {domain && <Domain>{domain}</Domain>}
+          {description && (
+            <Text style={{ marginTop: 8 }} weight={500} size="medium">
+              {description}
+            </Text>
+          )}
+          <SizedBox height={16} />
+          {buttons.length > 0 && (
+            <ButtonsContainer style={{ flexDirection: buttonsDirection }}>
+              {buttons?.map((Component, index) => <Component key="index" />)}
+            </ButtonsContainer>
+          )}
+        </Column>
+        <SizedBox height={24} />
+      </Root>
+    </Dialog>
+  );
 };
 
 type TSuccessBidPlacement = {
-	domain: string;
-	onBackToBids: () => void;
-	onExplorerClick: () => void;
+  domain: string;
+  onBackToBids: () => void;
+  onExplorerClick: () => void;
 };
 export const buildSuccessBidPlacementParams = ({
-	                                               domain,
-	                                               onBackToBids,
-	                                               onExplorerClick,
-                                               }: TSuccessBidPlacement): IDialogNotificationProps => {
-	return {
-		title: "You’ve successfully placed a bid on",
-		domain,
-		description: "You can track your bids on the main page of Name Service",
-		type: "success",
-		buttons: [
-			() => (
-				<Button size="medium" onClick={onBackToBids}>
-					Show My Bids
-				</Button>
-			),
-			() => (
-				<Button size="medium" onClick={onExplorerClick} kind="secondary">
-					View in Explorer
-				</Button>
-			),
-		],
-	};
+  domain,
+  onBackToBids,
+  onExplorerClick,
+}: TSuccessBidPlacement): IDialogNotificationProps => {
+  return {
+    title: "You’ve successfully placed a bid on",
+    domain,
+    description: "You can track your bids on the main page of Name Service",
+    type: "success",
+    buttons: [
+      () => (
+        <Button size="medium" onClick={onBackToBids}>
+          Show My Bids
+        </Button>
+      ),
+      () => (
+        <Button size="medium" onClick={onExplorerClick} kind="secondary">
+          View in Explorer
+        </Button>
+      ),
+    ],
+  };
 };
 
 type TSuccessPurchase = {
-	domain: string;
-	onGoMarket: () => void;
-	onGoBack: () => void;
+  domain: string;
+  onGoMarket: () => void;
+  onGoBack: () => void;
 };
 
 export const buildSuccessPurchaseParams = ({
-	                                           domain,
-	                                           onGoMarket,
-	                                           onGoBack,
-                                           }: TSuccessPurchase): IDialogNotificationProps => {
-	return {
-
-		title: "You’ve successfully purchased ",
-		domain,
-		description: "You can find you NFT in your wallet on Puzzle Market",
-		type: "success",
-		buttons: [
-			() => (
-				<Button size="medium" onClick={onGoMarket}>
-					View in Thunder Marketplace
-				</Button>
-			),
-			() => (
-				<Button size="medium" onClick={onGoBack} kind="secondary">
-					Back to Name Service
-				</Button>
-			),
-		],
-	};
+  domain,
+  onGoMarket,
+  onGoBack,
+}: TSuccessPurchase): IDialogNotificationProps => {
+  return {
+    title: "You’ve successfully purchased ",
+    domain,
+    description: "You can find you NFT in your wallet on Puzzle Market",
+    type: "success",
+    buttons: [
+      () => (
+        <Button size="medium" onClick={onGoMarket}>
+          View in Thunder Marketplace
+        </Button>
+      ),
+      () => (
+        <Button size="medium" onClick={onGoBack} kind="secondary">
+          Back to Name Service
+        </Button>
+      ),
+    ],
+  };
 };
 
 export default DialogNotification;
--- src/screens/AchievementsScreen/NftPreview.ts
+++ src/screens/AchievementsScreen/NftPreview.ts
@@ -4,14 +4,11 @@ const NftPreview = styled.div<{ src?: string }>`
   width: 100%;
   border-radius: 8px;
   transition: transform 0.3s;
-  ${({src}) => src == null
-          ? "border: 2px solid #000"
-          : `background: url(${src}) center no-repeat`};
+  ${({ src }) => (src == null ? "border: 2px solid #000" : `background: url(${src}) center no-repeat`)};
   background-size: cover;
 
   &:hover {
     transform: scale(1.01)
 `;
 
-
-export default NftPreview
+export default NftPreview;
